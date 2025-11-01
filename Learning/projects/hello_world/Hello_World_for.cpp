c260d7f4620d70f44843c59229a3625ba1e1b825
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-11-01 22:17:40 +0530

    Enhance Hello World program with user input
    Added user input for number of times to print the message.

--- Learning/projects/hello_world/Hello_World_for.cpp
+++ Learning/projects/hello_world/Hello_World_for.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 int main( void ) {
+    
     int n(0);
     std::cout << "Please enter a number: ";
     std::cin >> n;
     constexpr const char* str = " Hello, World !";
+    
     for( int  count = 0; count < n ; count++ ){
         std::cout << str << "\n";
     }

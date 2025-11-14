a30148d575ce22660e808bd12cc6eee3714fba14
reyon-mars <reyonrai50@gmail.com>
2025-11-14 16:18:04 +0545

    *

--- Learning/concepts/c++_primer/containers/string/string_insert.cpp
+++ Learning/concepts/c++_primer/containers/string/string_insert.cpp
@@ -13,6 +13,11 @@ std::string replace_idx( std::string& s, const std::string& old_val, const std::
 int main() {
     std::string prefix = "mr";
     std::string suffix = "smith";
+
+    std::string concat = prefix + suffix;
+
+    std::cout << concat << std::endl;
+
     std::string text = "Hello Mars";
     replace_idx(text, "Mars", "World");
     std::cout << text << '\n';  // Output: Hello World

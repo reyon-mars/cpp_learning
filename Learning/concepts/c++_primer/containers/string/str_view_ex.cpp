6c80f253784bdcd4b05355edd9f6f313144f3b2f
reyon-mars <reyonrai50@gmail.com>
2025-11-19 17:49:35 +0545

    Implemented the two log_msg functions.

---
+++ Learning/concepts/c++_primer/containers/string/str_view_ex.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+void log_msg( std::string_view tag, std::string_view msg )
+{
+    std::cout << '[' << tag << ']' << msg << '\n';
+    return ;
+}
+
+void log_msg_( std::string tag , std::string msg )
+{
+    std::cout << '[' << tag << ']' << msg << '\n';
+    return ;
+}
+
+
+int main ( void ){
+    
+}

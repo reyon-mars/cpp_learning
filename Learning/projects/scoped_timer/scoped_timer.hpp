bd2d6f3ed65d30c260437f3200a66291f3fb5d34
reyon-mars <reyonrai50@gmail.com>
2025-11-21 18:03:16 +0545

    Created the header file for the scoped_timer as well.:

---
+++ Learning/projects/scoped_timer/scoped_timer.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+
+
+class scoped_timer {
+    
+    private:
+    std::chrono::steady_clock::time_point start;
+    std::string name;
+    
+    public:
+    explicit scoped_timer( std::string str ) : start(std::chrono::steady_clock::now()), name( std::move(str )){}
+
+    ~scoped_timer(){
+        auto end = std::chrono::steady_clock::now();
+        std::cout << name << ':' << std::chrono::duration_cast<std::chrono::microseconds> ( end - start ).count() << '\n';
+    }
+};

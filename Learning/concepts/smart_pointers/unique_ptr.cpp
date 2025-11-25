9c6a14aa21bffb400d84f1d5a2eb06a8c07ae273
reyon-mars <reyonrai50@gmail.com>
2025-11-25 17:47:36 +0545

    Unique pointer practise.

---
+++ Learning/concepts/smart_pointers/unique_ptr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+
+struct User{
+    std::string name;
+    int age;
+};
+
+int main ( void ){
+    auto u = std::make_unique<User>( "Mars", 22 );
+
+    std::cout << u->name << " " << u->age << '\n';
+
+    std::unique_ptr<User> v;
+    v = std::move( u );
+
+    if( u == nullptr ){
+        std::cout << "U is nullptr ";
+    }
+    return 0;
+
+}

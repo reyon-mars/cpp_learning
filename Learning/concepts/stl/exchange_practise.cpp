47193d59a167113918a2955be8fd7df3384d5d30
reyon-mars <reyonrai50@gmail.com>
2025-11-27 17:38:14 +0545

    std::exchange practise.

---
+++ Learning/concepts/stl/exchange_practise.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <utility>
+
+class counter{
+    private:
+    int value;
+
+    public:
+
+    int increment( int amount ){
+        return std::exchange( value, value + amount );
+    }
+
+    int reset ( ){
+        return std::exchange( value, 0 );
+    }
+};
+
+
+int main ( void ){
+
+    counter countr;
+    for( int i = 0; i < 100; i++ ){
+        std::cout << countr.increment( i ) << " ";
+    }
+    std::cout << countr.reset(); 
+    return 0;
+
+}
+

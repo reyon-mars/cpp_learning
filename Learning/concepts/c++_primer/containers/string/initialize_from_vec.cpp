30c9f279ad3768049531695c344ac5a94f6fdb3f
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-11-28 20:23:54 +0545

    Initialize string from vector and print
    Added characters to vector and printed string created from it.

--- Learning/concepts/c++_primer/containers/string/initialize_from_vec.cpp
+++ Learning/concepts/c++_primer/containers/string/initialize_from_vec.cpp
@@ -5,14 +5,24 @@
 int main ( void ){
     std::vector<char> v;
 
+    // --- added lines ---
+    v.push_back('H');
+    v.push_back('i');
+    v.push_back('!');
+    // --------------------
+
     std::string str_v( v.begin(), v.end() );
     std::string str;
-
 
     for( auto c : v )
         str += c;
         
     std::cout << str;
-    
+
+    // --- added lines ---
+    std::cout << "\nString created using constructor: " << str_v << std::endl;
+    // --------------------
+
     return 0;
-} 
+}
+

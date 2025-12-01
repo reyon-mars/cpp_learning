4ff96665a6c7b1a4836f9cb10ea18166f16d5dba
reyon-mars <reyonrai50@gmail.com>
2025-12-01 17:52:52 +0545

    int range iterator updated.

--- Learning/concepts/iterators/int_range.cpp
+++ Learning/concepts/iterators/int_range.cpp
@@ -5,4 +5,34 @@ class int_range{
 
     public:
     
-}
+    class Iterator {
+        private:
+        int current_;
+
+        public:
+        explicit Iterator( int current ) : current_( current ){};
+        
+        int operator*()const{
+            return current_;
+        }
+        
+        Iterator& operator++( ){
+            ++current_;
+            return *this;
+        }
+
+        bool operator!=( const Iterator& other ){
+            return current_ != other.current_;
+        }
+    };
+
+    explicit int_range( int s, int e ) : start(s), end(e) {};
+    
+    Iterator begin(){
+        return Iterator( start );
+    }
+    Iterator end() {
+        return Iterator( end );
+    }
+
+};

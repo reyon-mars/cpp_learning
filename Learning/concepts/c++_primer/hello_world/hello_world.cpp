d9acda890f4cf055a443da1ff6926bf92f19f59a
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-12-06 21:29:31 +0545

    Update hello_world.cpp

--- Learning/concepts/c++_primer/hello_world/hello_world.cpp
+++ Learning/concepts/c++_primer/hello_world/hello_world.cpp
@@ -5,3 +5,18 @@ int main( void ){
     return 0;
     
 }
+
+void greet(){
+    std::cout << "This is extra code added without modifying main!" << std::endl;
+}
+
+int add(int a, int b){
+    return a + b;
+}
+
+class Demo{
+public:
+    void show(){
+        std::cout << "Demo class method executed!" << std::endl;
+    }
+};

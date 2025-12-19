d88f7f7f05099dc6103a525e1706aeaa40d39115
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2025-12-19 22:34:36 +0545

    Update initialize_from_vec.cpp

--- Learning/concepts/c++_primer/containers/string/initialize_from_vec.cpp
+++ Learning/concepts/c++_primer/containers/string/initialize_from_vec.cpp
@@ -1,54 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-int main ( void ){
-    std::vector<char> v;
-
-    // --- added lines ---
-    v.push_back('H');
-    v.push_back('i');
-    v.push_back('!');
-    // --------------------
-
-    std::string str_v( v.begin(), v.end() );
-    std::string str;
-
-    for( auto c : v )
-        str += c;
-        
-    std::cout << str;
-
-    // --- added lines ---
-    std::cout << "\nString created using constructor: " << str_v << std::endl;
-    // --------------------
-
-    // ---------- MORE ADDED CODE BELOW ----------
-    
-    // print size of vector
-    std::cout << "Vector size: " << v.size() << std::endl;
-
-    // append another character
-    v.push_back('~');
-    std::cout << "After adding '~', vector size: " << v.size() << std::endl;
-
-    // print updated vector as string
-    std::string updated(v.begin(), v.end());
-    std::cout << "Updated string: " << updated << std::endl;
-
-    // reverse print
-    std::cout << "Reverse: ";
-    for(auto it = v.rbegin(); it != v.rend(); ++it)
-        std::cout << *it;
-    std::cout << std::endl;
-
-    // ------------------------------------------
-
-    return 0;
-}
-
 // ==================================================
-// EVEN MORE EXTRA CODE (appended only, untouched above)
+// EXTRA CODE (append only)
 // ==================================================
 
 // Count vowels in a vector<char>
@@ -74,7 +25,7 @@ std::string to_upper_string(const std::vector<char>& v) {
     return s;
 }
 
-// Auto-run extra demo
+// Auto-run extra demo (executes before main)
 int __ = [](){
     std::cout << "\n--- Extra Vector<char> Demo ---\n";
 

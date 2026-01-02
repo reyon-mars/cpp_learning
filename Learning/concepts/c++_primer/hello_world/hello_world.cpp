6545c2f5b3333238601b68f06ced38ff18f0934a
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-01-02 21:57:35 +0545

    Update hello_world.cpp

--- Learning/concepts/c++_primer/hello_world/hello_world.cpp
+++ Learning/concepts/c++_primer/hello_world/hello_world.cpp
@@ -21,78 +21,26 @@ public:
 };
 
 // ======================================================
-// EXTRA STRUCTS / CLASSES
+// SMALL ADDITIONAL CODE
 // ======================================================
 
-struct ExtraStruct {
-    int x = 42;
+// Simple struct
+struct Info {
+    int value = 10;
 };
 
-class MoreDemo {
-public:
-    void info() {
-        std::cout << "MoreDemo class executing additional logic!" << std::endl;
-    }
-};
-
-struct Counter {
-    int value = 100;
-};
-
-class FinalDemo {
-public:
-    void run() {
-        std::cout << "FinalDemo::run() executed!" << std::endl;
-    }
-};
-
-// ======================================================
-// TEMPLATE + RAII
-// ======================================================
-
-template<typename T>
-T multiply(T a, T b) {
-    return a * b;
+// Simple helper function
+void printMessage(const std::string& msg) {
+    std::cout << msg << std::endl;
 }
 
-struct LifetimeTracer {
-    std::string name;
-
-    LifetimeTracer(std::string n) : name(std::move(n)) {
-        std::cout << "[Lifetime] Construct " << name << std::endl;
-    }
-
-    ~LifetimeTracer() {
-        std::cout << "[Lifetime] Destruct " << name << std::endl;
+// Simple class
+class ExtraDemo {
+public:
+    void run() {
+        std::cout << "ExtraDemo running successfully!" << std::endl;
     }
 };
-
-// ======================================================
-// AUTO-RUN EXTRA DEMOS (BEFORE MAIN)
-// ======================================================
-
-int __pre_main_extra = [](){
-    std::cout << "\n[Pre-main Extra Demo]\n";
-
-    ExtraStruct es;
-    std::cout << "Pre-main ExtraStruct.x = " << es.x << std::endl;
-
-    MoreDemo more;
-    more.info();
-
-    Counter counter;
-    std::cout << "Pre-main Counter value = " << counter.value << std::endl;
-
-    FinalDemo finalDemo;
-    finalDemo.run();
-
-    std::cout << "multiply(3, 4) = " << multiply(3, 4) << std::endl;
-
-    LifetimeTracer tracer("PreMainTracer");
-    std::cout << "RAII object alive inside pre-main lambda\n";
-
-    return 0;
-}();
 
 // ======================================================
 // MAIN
@@ -103,39 +51,21 @@ int main(void) {
     // ---------- ORIGINAL ----------
     std::cout << "Hello, World" << std::endl;
 
-    // ---------- EXTRA FUNCTION CALLS ----------
     greet();
     std::cout << "5 + 7 = " << add(5, 7) << std::endl;
 
-    // ---------- EXTRA STRUCT ----------
-    ExtraStruct es;
-    std::cout << "ExtraStruct.x = " << es.x << std::endl;
-
-    // ---------- CLASS DEMOS ----------
     Demo demo;
     demo.show();
 
-    MoreDemo more;
-    more.info();
+    // ---------- SMALL EXTRA ----------
+    Info info;
+    std::cout << "Info value = " << info.value << std::endl;
 
-    // ---------- COUNTER ----------
-    Counter counter;
-    std::cout << "Counter value = " << counter.value << std::endl;
+    printMessage("This is a simple helper function");
 
-    // ---------- FINAL DEMO ----------
-    FinalDemo finalDemo;
-    finalDemo.run();
+    ExtraDemo extra;
+    extra.run();
 
-    // ---------- TEMPLATE ----------
-    std::cout << "multiply(6, 7) = " << multiply(6, 7) << std::endl;
-
-    // ---------- RAII ----------
-    {
-        LifetimeTracer tracer("MainScopeTracer");
-        std::cout << "RAII object alive inside scope\n";
-    }
-
-    std::cout << "End of program execution." << std::endl;
+    std::cout << "Program finished successfully." << std::endl;
     return 0;
 }
- 

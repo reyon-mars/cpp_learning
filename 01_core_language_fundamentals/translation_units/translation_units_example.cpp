2e95a42a5e4658d019f4587d1db5f2a1e7cfcc5a
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-02-24 14:42:22 +0545

    Update translation_units_example.cpp

--- 01_core_language_fundamentals/translation_units/translation_units_example.cpp
+++ 01_core_language_fundamentals/translation_units/translation_units_example.cpp
@@ -6,6 +6,13 @@
 // Forward declaration
 void function_from_another_unit();
 
+// ---------------- SMALL ADDITION ----------------
+// (Normally this would live in another .cpp file)
+void function_from_another_unit() {
+    std::cout << "Function from another translation unit\n";
+}
+// ------------------------------------------------
+
 int main() {
     std::cout << "Translation unit example\n";
     function_from_another_unit();

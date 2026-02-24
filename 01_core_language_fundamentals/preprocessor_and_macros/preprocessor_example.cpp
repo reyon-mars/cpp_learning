1f84c80180795d10291ef22b309cbc3519086459
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-02-24 14:26:31 +0545

    Update preprocessor_example.cpp

--- 01_core_language_fundamentals/preprocessor_and_macros/preprocessor_example.cpp
+++ 01_core_language_fundamentals/preprocessor_and_macros/preprocessor_example.cpp
@@ -4,16 +4,45 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
 
+#include <iostream>
+
+// ---------------- ORIGINAL MACROS ----------------
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define STRINGIFY(x) #x
 #define CONCAT(a, b) a ## b
+// ------------------------------------------------
 
-#include <iostream>
+// -------- SMALL ADDITIONS --------
+
+// simple numeric macro
+#define SQUARE(x) ((x) * (x))
+
+// feature toggle macro
+#define DEBUG_MODE
+
+#ifdef DEBUG_MODE
+    #define DEBUG_PRINT(msg) std::cout << "[DEBUG] " << msg << "\n"
+#else
+    #define DEBUG_PRINT(msg)
+#endif
+
+// ---------------------------------
 
 int main() {
+
     std::cout << "Max of 5 and 10: " << MAX(5, 10) << "\n";
     std::cout << "Stringify: " << STRINGIFY(hello) << "\n";
+
+    // ---- small added usage ----
+    std::cout << "Square of 4: " << SQUARE(4) << "\n";
+
+    DEBUG_PRINT("Debug mode is enabled");
+
+    int CONCAT(var, 1) = 42;
+    std::cout << "Concatenated variable value: " << var1 << "\n";
+    // ---------------------------
+
     return 0;
 }
 
-#endif
+#endif // MY_HEADER_H

a2dad33f79e81a9d87bbc85c8806f880c651a669
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-02-27 22:40:17 +0545

    Update object_as_parameter.cpp

--- 03_classes_and_oop/member_functions/object_as_parameter.cpp
+++ 03_classes_and_oop/member_functions/object_as_parameter.cpp
@@ -3,96 +3,126 @@
 using namespace std;
 
 class Pet {
-  protected:
+protected:
     string name;
-  public:
-    Pet( string name ) : name( name ) {}
-    void NameMe( string name ) { this -> name = name;  return ; }
-    virtual void MakeSound( void ) { cout << name << " says Objects " << endl; return ; }
+public:
+    Pet(string name) : name(name) {}
+
+    virtual ~Pet() {}   // 🔹 added virtual destructor
+
+    void NameMe(string name) { this->name = name; }
+
+    virtual void MakeSound(void) {
+        cout << name << " says Objects " << endl;
+    }
 };
 
-//Declaring a child class of the Pet Superclass called dog
-class Dog: public Pet {
-  public:
-    Dog( string name ) : Pet( name ) {}
-    void MakeSound( void ) { cout << name << " says woof woof woof " << endl; return ; }
+// Declaring a child class of the Pet Superclass called Dog
+class Dog : public Pet {
+public:
+    Dog(string name) : Pet(name) {}
+
+    void MakeSound(void) override {   // 🔹 added override
+        cout << name << " says woof woof woof " << endl;
+    }
 };
 
-class GermanShepherd: public Dog {
-  public:
-    GermanShepherd( string name ) : Dog( name ) {}
-    void MakeSound( void ) { cout << name << " says wuff wuff wuff " << endl; return ; }
-	void Laufen( void ) { cout << name << " the german shepherd is running " << endl; return ; }
+class GermanShepherd : public Dog {
+public:
+    GermanShepherd(string name) : Dog(name) {}
+
+    void MakeSound(void) override {
+        cout << name << " says wuff wuff wuff " << endl;
+    }
+
+    void Laufen(void) {
+        cout << name << " the german shepherd is running " << endl;
+    }
 };
 
 class MastinEspanol : public Dog {
-  public:
-    MastinEspanol( string name ) : Dog( name ) {}
-    void MakeSound( void ) { cout << name << " says Guau Guau Guau " << endl; return ; }
-	void Ejectuar( void ) { cout << name << " the Mastin Espanol is running " << endl; return ; }
+public:
+    MastinEspanol(string name) : Dog(name) {}
+
+    void MakeSound(void) override {
+        cout << name << " says Guau Guau Guau " << endl;
+    }
+
+    void Ejectuar(void) {
+        cout << name << " the Mastin Espanol is running " << endl;
+    }
 };
 
-void PlayWithPet( Pet &pet ) {
-  GermanShepherd *gs;
-  MastinEspanol *mes;
-  if( ( gs = dynamic_cast<GermanShepherd * >( &pet ) ) ){ // using the dynamic_cast to check whether the Pet* can be converted to GermanShepherd *
-    gs-> Laufen();
-  }
-  try{
-    dynamic_cast<MastinEspanol &>(pet).Ejectuar(); // using dynamic_cast to try to convert the Pet reference to MastinEspanol reference
-    }catch(...){} // if the casting fails catch the error and do nothing
-  pet.MakeSound();
-  return ;
+void PlayWithPet(Pet &pet) {
+    GermanShepherd *gs;
+    MastinEspanol *mes;
+
+    if ((gs = dynamic_cast<GermanShepherd*>(&pet))) {
+        gs->Laufen();
+    }
+
+    try {
+        dynamic_cast<MastinEspanol&>(pet).Ejectuar();
+    }
+    catch (...) {}
+
+    pet.MakeSound();
 }
 
-void PlayWithPet( Pet *pet ){
-  pet->MakeSound();
-  return ;
+void PlayWithPet(Pet *pet) {
+    if (!pet) return;   // 🔹 small safety check
+
+    pet->MakeSound();
 }
 
-// Declaring a function that takes an object by value
-void PlayWithPetByValue( string name , Pet pet ){
-  pet.NameMe( name );
-  pet.MakeSound();
-  return ;
-
-}
-// Declaring a functoin that takes an object as a pointer
-void PlayWithPetByPointer( string name, Pet *pet ){
-  pet -> NameMe( name );
-  pet -> MakeSound();
-  return ;
-}
-// Declaring a function that takes an object by reference
-void PlayWithPetByReference( string name, Pet &pet ){
-  pet.NameMe( name );
-  pet.MakeSound();
+void PlayWithPetByValue(string name, Pet pet) {
+    pet.NameMe(name);
+    pet.MakeSound();
 }
 
-int main( void ) {
-  Pet petReference("Almonds") , *petPointer = new Pet("Max");
-  PlayWithPetByPointer( "anonymous" , petPointer );
-  PlayWithPetByReference( "anonymous" , petReference );
-  PlayWithPetByPointer( "no_name", &petReference );
-  PlayWithPetByReference( "no_name", *petPointer );
+void PlayWithPetByPointer(string name, Pet *pet) {
+    pet->NameMe(name);
+    pet->MakeSound();
+}
 
-  Pet pet(" Animal " );
-  Pet *pet_ptr = &pet;
-  Dog dog(" Dog " );
-  Dog *dog_ptr = &dog;
-  GermanShepherd gs( " Hund " );
-  GermanShepherd *gs_ptr = &gs;
-  MastinEspanol mes( " Perro " );
-  MastinEspanol *mes_ptr = &mes;
-  PlayWithPet( pet );
-  PlayWithPet( dog );
-  PlayWithPet( gs );
-  PlayWithPet( mes );
-  PlayWithPet( pet_ptr );
-  PlayWithPet( dog_ptr );
-  PlayWithPet( gs_ptr );
-  PlayWithPet( mes_ptr );
+void PlayWithPetByReference(string name, Pet &pet) {
+    pet.NameMe(name);
+    pet.MakeSound();
+}
 
-  return 0;
+int main(void) {
 
+    Pet petReference("Almonds"),
+        *petPointer = new Pet("Max");
+
+    PlayWithPetByPointer("anonymous", petPointer);
+    PlayWithPetByReference("anonymous", petReference);
+    PlayWithPetByPointer("no_name", &petReference);
+    PlayWithPetByReference("no_name", *petPointer);
+
+    Pet pet("Animal");
+    Pet *pet_ptr = &pet;
+
+    Dog dog("Dog");
+    Dog *dog_ptr = &dog;
+
+    GermanShepherd gs("Hund");
+    GermanShepherd *gs_ptr = &gs;
+
+    MastinEspanol mes("Perro");
+    MastinEspanol *mes_ptr = &mes;
+
+    PlayWithPet(pet);
+    PlayWithPet(dog);
+    PlayWithPet(gs);
+    PlayWithPet(mes);
+
+    PlayWithPet(pet_ptr);
+    PlayWithPet(dog_ptr);
+    PlayWithPet(gs_ptr);
+    PlayWithPet(mes_ptr);
+
+    delete petPointer;   // 🔹 cleanup
+
+    return 0;
 }

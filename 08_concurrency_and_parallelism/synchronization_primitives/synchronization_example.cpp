bff362249f8a3dffbc50e917cc820b0cd6016b83
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-09 14:15:50 +0545

    Update synchronization_example.cpp

--- 08_concurrency_and_parallelism/synchronization_primitives/synchronization_example.cpp
+++ 08_concurrency_and_parallelism/synchronization_primitives/synchronization_example.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 std::mutex mtx;
+std::mutex mtx2;
 int shared_data = 0;
 
 void increment_safe() {
@@ -23,18 +24,45 @@ void increment_with_unique_lock() {
     std::cout << "Lock released\n";
 }
 
+void try_increment() {
+    if (mtx.try_lock()) {
+        shared_data += 10;
+        std::cout << "try_lock succeeded\n";
+        mtx.unlock();
+    } else {
+        std::cout << "try_lock failed\n";
+    }
+}
+
+// Demonstrates locking multiple mutexes safely
+void safe_dual_lock() {
+    std::scoped_lock lock(mtx, mtx2);
+    shared_data += 20;
+}
+
 int main() {
+
     std::vector<std::thread> threads;
-    
+
+    // lock_guard threads
     for (int i = 0; i < 5; ++i) {
         threads.emplace_back(increment_safe);
     }
-    
+
+    // unique_lock thread
+    threads.emplace_back(increment_with_unique_lock);
+
+    // try_lock thread
+    threads.emplace_back(try_increment);
+
+    // scoped_lock thread
+    threads.emplace_back(safe_dual_lock);
+
     for (auto& t : threads) {
         t.join();
     }
-    
+
     std::cout << "Final value: " << shared_data << "\n";
-    
+
     return 0;
 }

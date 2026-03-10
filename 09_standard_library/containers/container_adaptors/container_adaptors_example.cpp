70b1a79414199313e6cb040af5ef40facb231420
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-10 22:53:12 +0545

    Update container_adaptors_example.cpp

--- 09_standard_library/containers/container_adaptors/container_adaptors_example.cpp
+++ 09_standard_library/containers/container_adaptors/container_adaptors_example.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+#include <vector>
+#include <functional>
 
 int main() {
     // Stack
@@ -38,6 +40,31 @@ int main() {
         std::cout << "PQ: " << pq.top() << "\n";
         pq.pop();
     }
-    
+
+    // ---- Additional small examples ----
+
+    // stack size example
+    s.push(7);
+    s.push(8);
+    std::cout << "Stack size: " << s.size() << "\n";
+
+    // queue empty check
+    std::cout << "Is queue empty? " << (q.empty() ? "Yes" : "No") << "\n";
+
+    // min priority_queue
+    std::priority_queue<int, std::vector<int>, std::greater<int>> min_pq;
+    min_pq.push(5);
+    min_pq.push(10);
+    min_pq.push(3);
+
+    std::cout << "Min Priority Queue order: ";
+    while (!min_pq.empty()) {
+        std::cout << min_pq.top() << " ";
+        min_pq.pop();
+    }
+    std::cout << "\n";
+
+    // -----------------------------------
+
     return 0;
 }

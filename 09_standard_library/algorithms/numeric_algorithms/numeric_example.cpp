05bdf0dfad3dfe661f395fefffd951ce5a31b46a
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-10 22:51:09 +0545

    Update numeric_example.cpp

--- 09_standard_library/algorithms/numeric_algorithms/numeric_example.cpp
+++ 09_standard_library/algorithms/numeric_algorithms/numeric_example.cpp
@@ -22,6 +22,24 @@ int main() {
     int dot_product = std::inner_product(vec.begin(), vec.end(), 
                                          vec2.begin(), 0);
     std::cout << "Dot product: " << dot_product << "\n";
+
+    // partial_sum
+    std::vector<int> partial(vec.size());
+    std::partial_sum(vec.begin(), vec.end(), partial.begin());
+    std::cout << "Partial sums: ";
+    for (int v : partial) {
+        std::cout << v << " ";
+    }
+    std::cout << "\n";
+
+    // adjacent_difference
+    std::vector<int> diff(vec.size());
+    std::adjacent_difference(vec.begin(), vec.end(), diff.begin());
+    std::cout << "Adjacent differences: ";
+    for (int v : diff) {
+        std::cout << v << " ";
+    }
+    std::cout << "\n";
     
     return 0;
 }

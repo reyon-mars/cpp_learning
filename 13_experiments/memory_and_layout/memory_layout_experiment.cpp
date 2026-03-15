c41eb4a02327934f5562aefede5d7f3c49b5625e
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-15 18:55:12 +0545

    Update memory_layout_experiment.cpp

--- 13_experiments/memory_and_layout/memory_layout_experiment.cpp
+++ 13_experiments/memory_and_layout/memory_layout_experiment.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cstring>
+#include <cstddef>
 
 struct Unaligned {
     char c;      // 1 byte
@@ -16,14 +17,37 @@ struct Aligned {
     char d;      // 1 byte
 };
 
+// Small packed example
+#pragma pack(push, 1)
+struct Packed {
+    char c;
+    int i;
+    char d;
+};
+#pragma pack(pop)
+
 int main() {
+
     std::cout << "Size of Unaligned: " << sizeof(Unaligned) << "\n";
     std::cout << "Size of Aligned: " << sizeof(Aligned) << "\n";
-    
-    std::cout << "Offset in Unaligned:\n";
+    std::cout << "Size of Packed: " << sizeof(Packed) << "\n";
+
+    std::cout << "\nAlignment requirements:\n";
+    std::cout << "alignof(Unaligned): " << alignof(Unaligned) << "\n";
+    std::cout << "alignof(Aligned): " << alignof(Aligned) << "\n";
+
+    std::cout << "\nOffset in Unaligned:\n";
     std::cout << "  c: " << offsetof(Unaligned, c) << "\n";
     std::cout << "  i: " << offsetof(Unaligned, i) << "\n";
     std::cout << "  d: " << offsetof(Unaligned, d) << "\n";
-    
+
+    // Small memory layout visualization
+    Unaligned obj{};
+    std::cout << "\nMemory addresses:\n";
+    std::cout << "  &obj: " << &obj << "\n";
+    std::cout << "  &obj.c: " << static_cast<void*>(&obj.c) << "\n";
+    std::cout << "  &obj.i: " << static_cast<void*>(&obj.i) << "\n";
+    std::cout << "  &obj.d: " << static_cast<void*>(&obj.d) << "\n";
+
     return 0;
 }

a2eb9934541e32379c181b7a7ccfc3038901058c
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-16 20:57:17 +0545

    Update open_gl_test.cpp

--- 14_graphics_and_visualization/opengl_fundamentals/open_gl_test.cpp
+++ 14_graphics_and_visualization/opengl_fundamentals/open_gl_test.cpp
@@ -1,24 +1,41 @@
-#include <OpenGL/gl3.h>
-#include <GLUT/glut.h>
+#include <GL/glut.h>   // simpler header for GLUT
+#include <iostream>
 
 void display() {
+
     glClear(GL_COLOR_BUFFER_BIT);
+
     glBegin(GL_TRIANGLES);
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex2f(-0.5, -0.5);
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex2f(0.5, -0.5);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex2f(0.0, 0.5);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(-0.5f, -0.5f);
+
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(0.5f, -0.5f);
+
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(0.0f, 0.5f);
+
     glEnd();
+
     glFlush();
 }
 
 int main(int argc, char** argv) {
+
     glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+
     glutCreateWindow("OpenGL Test");
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+
     glutDisplayFunc(display);
+
     glutMainLoop();
+
     return 0;
 }
-

af93442c10559bb718c33b3b16584a42a003a8b2
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-17 10:54:05 +0545

    Update compilation_model_example.cpp

--- 01_core_language_fundamentals/compilation_model/compilation_model_example.cpp
+++ 01_core_language_fundamentals/compilation_model/compilation_model_example.cpp
@@ -8,6 +8,12 @@
 // Preprocessor macro
 #define APP_VERSION "1.0"
 
+// Conditional compilation
+#define DEBUG_MODE
+
+// Function prototype (parsing stage example)
+void debugMessage();
+
 // Simple helper function (compiled separately conceptually)
 void printVersion() {
     std::cout << "App Version: " << APP_VERSION << "\n";
@@ -15,6 +21,24 @@ void printVersion() {
 
 // Global variable (linking example)
 int build_number = 1;
+
+// Static variable (internal linkage)
+static int internal_counter = 0;
+
+// Inline function (compiler optimization)
+inline int square(int x) {
+    return x * x;
+}
+
+// Function definition (after prototype)
+void debugMessage() {
+#ifdef DEBUG_MODE
+    std::cout << "[DEBUG] Debug mode is ON\n";
+#endif
+}
+
+// Simulating extern usage
+extern int build_number;
 
 // ------------------------------------
 
@@ -24,6 +48,15 @@ int main() {
     // Added usage
     printVersion();
     std::cout << "Build number: " << build_number << "\n";
+
+    // -------- NEW ADDITIONS USAGE --------
+    debugMessage();
+
+    internal_counter++;
+    std::cout << "Internal counter: " << internal_counter << "\n";
+
+    std::cout << "Square of 5: " << square(5) << "\n";
+    // ------------------------------------
 
     return 0;
 }

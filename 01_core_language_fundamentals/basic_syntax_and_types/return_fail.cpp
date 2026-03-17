d0670987951d5bcec48a504d9a38d115ac8987d6
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-17 10:48:46 +0545

    Update return_fail.cpp

--- 01_core_language_fundamentals/basic_syntax_and_types/return_fail.cpp
+++ 01_core_language_fundamentals/basic_syntax_and_types/return_fail.cpp
@@ -5,12 +5,13 @@ int main(void) {
 
     int status = -1;
 
-    if (status != 0) {
+    if (status != EXIT_SUCCESS) {
         std::cout << "Program exited with error code: "
                   << status << std::endl;
     } else {
         std::cout << "Program exited successfully." << std::endl;
     }
 
-    return status;
+    // Optional: normalize return value
+    return (status == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }

5445e0a29571bea8d401427c0f1d4ba4bc50bd47
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-22 12:37:06 +0545

    Update multiple_inheritance_example.cpp

--- 03_classes_and_oop/multiple_inheritance/multiple_inheritance_example.cpp
+++ 03_classes_and_oop/multiple_inheritance/multiple_inheritance_example.cpp
@@ -5,29 +5,33 @@
 
 class A {
 public:
-    A() { std::cout << "A constructed\n"; }  // 🔹 added
+    A() { std::cout << "A constructed\n"; }
+    virtual ~A() { std::cout << "A destroyed\n"; }   // 🔹 added
     virtual void method() { std::cout << "A::method\n"; }
 };
 
 class B : virtual public A {
 public:
-    B() { std::cout << "B constructed\n"; }  // 🔹 added
+    B() { std::cout << "B constructed\n"; }
+    ~B() { std::cout << "B destroyed\n"; }           // 🔹 added
     void method() override { std::cout << "B::method\n"; }
 };
 
 class C : virtual public A {
 public:
-    C() { std::cout << "C constructed\n"; }  // 🔹 added
+    C() { std::cout << "C constructed\n"; }
+    ~C() { std::cout << "C destroyed\n"; }           // 🔹 added
     void method() override { std::cout << "C::method\n"; }
 };
 
 class D : public B, public C {
 public:
-    D() { std::cout << "D constructed\n"; }  // 🔹 added
+    D() { std::cout << "D constructed\n"; }
+    ~D() { std::cout << "D destroyed\n"; }           // 🔹 added
 
     void method() override { std::cout << "D::method\n"; }
 
-    void callBaseVersions() {               // 🔹 added helper
+    void callBaseVersions() {
         B::method();
         C::method();
         A::method();
@@ -43,12 +47,19 @@ int main() {
 
     std::cout << "---- Through A reference ----\n";
     A& a = obj;
-    a.method();   // virtual dispatch → D::method
+    a.method();
+
+    std::cout << "---- Through B pointer ----\n";   // 🔹 added
+    B* b = &obj;
+    b->method();
+
+    std::cout << "---- Through C pointer ----\n";   // 🔹 added
+    C* c = &obj;
+    c->method();
 
     std::cout << "---- Explicit base calls ----\n";
     obj.callBaseVersions();
 
-    // 🔹 Proving single A instance (virtual inheritance)
     B* bPtr = &obj;
     C* cPtr = &obj;
 

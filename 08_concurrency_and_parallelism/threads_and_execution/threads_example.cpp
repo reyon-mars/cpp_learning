ae3063a5bbcba0d7811716e9856349802a036f9f
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-03-28 20:37:59 +0545

    Update threads_example.cpp

--- 08_concurrency_and_parallelism/threads_and_execution/threads_example.cpp
+++ 08_concurrency_and_parallelism/threads_and_execution/threads_example.cpp
@@ -5,10 +5,13 @@
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <chrono>
 
 std::mutex mtx;
 std::mutex mtx2;
 int shared_data = 0;
+
+// ---------------- ORIGINAL FUNCTIONS ----------------
 
 void increment_safe() {
     std::lock_guard<std::mutex> lock(mtx);
@@ -40,6 +43,37 @@ void safe_dual_lock() {
     shared_data += 20;
 }
 
+// ---------------- SMALL ADDITIONS ----------------
+
+// Manual lock/unlock example
+void manual_increment() {
+    mtx.lock();
+    shared_data += 5;
+    std::cout << "manual lock used\n";
+    mtx.unlock();
+}
+
+// Timed attempt using try_lock with delay simulation
+void delayed_try_increment() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    if (mtx.try_lock()) {
+        shared_data += 15;
+        std::cout << "delayed try_lock succeeded\n";
+        mtx.unlock();
+    } else {
+        std::cout << "delayed try_lock failed\n";
+    }
+}
+
+// Thread-safe printing
+std::mutex cout_mtx;
+void safe_print(const std::string& msg) {
+    std::lock_guard<std::mutex> lock(cout_mtx);
+    std::cout << msg << "\n";
+}
+
+// ---------------- MAIN ----------------
+
 int main() {
 
     std::vector<std::thread> threads;
@@ -57,6 +91,14 @@ int main() {
 
     // scoped_lock thread
     threads.emplace_back(safe_dual_lock);
+
+    // ---------------- ADDED THREADS ----------------
+    threads.emplace_back(manual_increment);
+    threads.emplace_back(delayed_try_increment);
+
+    threads.emplace_back([]() {
+        safe_print("Thread-safe print demo");
+    });
 
     for (auto& t : threads) {
         t.join();

7a30d17026a0e8a040a4638e02e6693112e594f0
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-04-10 23:42:45 +0545

    Update multiple_inheritance_example.cpp

--- 03_classes_and_oop/multiple_inheritance/multiple_inheritance_example.cpp
+++ 03_classes_and_oop/multiple_inheritance/multiple_inheritance_example.cpp
@@ -6,28 +6,40 @@
 class A {
 public:
     A() { std::cout << "A constructed\n"; }
-    virtual ~A() { std::cout << "A destroyed\n"; }   // 🔹 added
+    virtual ~A() { std::cout << "A destroyed\n"; }
     virtual void method() { std::cout << "A::method\n"; }
+
+    // -------- EXTRA ADDITION --------
+    void identify() { std::cout << "I am A\n"; }
+    // --------------------------------
 };
 
 class B : virtual public A {
 public:
     B() { std::cout << "B constructed\n"; }
-    ~B() { std::cout << "B destroyed\n"; }           // 🔹 added
+    ~B() { std::cout << "B destroyed\n"; }
     void method() override { std::cout << "B::method\n"; }
+
+    // -------- EXTRA ADDITION --------
+    void identifyB() { std::cout << "I am B\n"; }
+    // --------------------------------
 };
 
 class C : virtual public A {
 public:
     C() { std::cout << "C constructed\n"; }
-    ~C() { std::cout << "C destroyed\n"; }           // 🔹 added
+    ~C() { std::cout << "C destroyed\n"; }
     void method() override { std::cout << "C::method\n"; }
+
+    // -------- EXTRA ADDITION --------
+    void identifyC() { std::cout << "I am C\n"; }
+    // --------------------------------
 };
 
 class D : public B, public C {
 public:
     D() { std::cout << "D constructed\n"; }
-    ~D() { std::cout << "D destroyed\n"; }           // 🔹 added
+    ~D() { std::cout << "D destroyed\n"; }
 
     void method() override { std::cout << "D::method\n"; }
 
@@ -36,6 +48,22 @@ public:
         C::method();
         A::method();
     }
+
+    // -------- EXTRA ADDITIONS --------
+    void identifyAll() {
+        identify();   // from A
+        identifyB();  // from B
+        identifyC();  // from C
+        std::cout << "I am D\n";
+    }
+
+    void printObjectLayout() {
+        std::cout << "Address of D: " << this << "\n";
+        std::cout << "Address as B: " << static_cast<B*>(this) << "\n";
+        std::cout << "Address as C: " << static_cast<C*>(this) << "\n";
+        std::cout << "Address as A: " << static_cast<A*>(this) << "\n";
+    }
+    // --------------------------------
 };
 
 int main() {
@@ -49,11 +77,11 @@ int main() {
     A& a = obj;
     a.method();
 
-    std::cout << "---- Through B pointer ----\n";   // 🔹 added
+    std::cout << "---- Through B pointer ----\n";
     B* b = &obj;
     b->method();
 
-    std::cout << "---- Through C pointer ----\n";   // 🔹 added
+    std::cout << "---- Through C pointer ----\n";
     C* c = &obj;
     c->method();
 
@@ -68,6 +96,16 @@ int main() {
 
     std::cout << "Address of A via C: "
               << static_cast<A*>(cPtr) << "\n";
+
+    // -------- EXTRA USAGE --------
+
+    std::cout << "---- Identity check ----\n";
+    obj.identifyAll();
+
+    std::cout << "---- Object layout ----\n";
+    obj.printObjectLayout();
+
+    // ----------------------------------
 
     return 0;
 }

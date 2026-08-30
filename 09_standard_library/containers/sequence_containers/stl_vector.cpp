ecb9beaa4b47c676b0daeba439027f7717f64094
Reyon Rai <reyonrai50@gmail.com>
2026-08-30 14:13:13 +0545

    add show_vectors for proper formatted triangle.

--- 09_standard_library/containers/sequence_containers/stl_vector.cpp
+++ 09_standard_library/containers/sequence_containers/stl_vector.cpp
@@ -1,19 +1,21 @@
 #include <algorithm>
+#include <format>
 #include <iostream>
 #include <iterator>
 #include <ostream>
 #include <print>
+#include <string>
 #include <vector>
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& triangle)
 {
-    for( const auto& row : triangle )
-    {
-        std::ranges::copy( row, std::ostream_iterator<T>( os, " " ));
-        os << '\n';
-    }
-    return os;
+	for (const auto& row : triangle)
+	{
+		std::ranges::copy(row, std::ostream_iterator<T>(os, " "));
+		os << '\n';
+	}
+	return os;
 }
 
 std::vector<int> get_next_row(const std::vector<int>& prev_row)
@@ -50,16 +52,37 @@ auto generate_triangle(int num_rows)
 	return triangle;
 }
 
+void show_vectors(std::ostream& os, std::vector<std::vector<int>>& v)
+{
+	size_t base_width = v.back().size();
+
+	std::string spaces(base_width * 3, ' ');
+
+	for (const auto& row : v)
+	{
+		os << spaces;
+		if (spaces.size() > 3)
+		{
+			spaces.resize(spaces.size() - 3);
+		}
+		for (const auto& data : row)
+		{
+			os << std::format("{: ^{}}", data, 6);
+		}
+		os << '\n';
+	}
+}
+
 int main()
 {
-    std::println("Please enter the number of rows: " );
-    int num_rows{};
+	std::println("Please enter the number of rows: ");
+	int num_rows{};
 
-    std::cin >> num_rows;
+	std::cin >> num_rows;
 
-	auto triangle {generate_triangle(num_rows)};
+	auto triangle{generate_triangle(num_rows)};
 
-    std::cout << triangle;
+    show_vectors( std::cout, triangle );
 
 	return 0;
 }

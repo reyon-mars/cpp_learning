4e8e8de482e426e0e92303c19dcbd4a63ad65a9e
Reyon Rai <reyonrai50@gmail.com>
2026-09-05 21:36:03 +0545

    ex_2.3 done.

--- 01_foundations/basic_syntax_and_types/ex_2.3.cpp
+++ 01_foundations/basic_syntax_and_types/ex_2.3.cpp
@@ -6,15 +6,15 @@ int main()
 	unsigned u{10}, u2{42};
 
 	std::println("Unsigned \n {}-{} : {} ", u, u2, (u - u2));
-	std::println("{}-{}: ", u2, u, (u2 - u));
+	std::println(" {}-{} : {} ", u2, u, (u2 - u));
 
 	int i{10}, i2{42};
 
 	std::println("Signed \n {}-{} : {}", i, i2, (i - i2));
-	std::println("{}-{} : {}", i2, i, (i2 - i));
+	std::println(" {}-{} : {}", i2, i, (i2 - i));
 
 	std::println("Mixed \n {} - {} : {} ", i, u, (i - u));
-	std::println("{}-{} : {}", u, i, (u - i));
+	std::println(" {}-{} : {}", u, i, (u - i));
 
 	return 0;
 }

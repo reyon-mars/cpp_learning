2e6c4b7b88d41e16e5825ec44e0ef476f616f997
Reyon Rai <reyonrai50@gmail.com>
2026-09-05 22:56:03 +0545

    refactor: use std::optional

--- 05_standard_library/random_generation/guess_the_num.cpp
+++ 05_standard_library/random_generation/guess_the_num.cpp
@@ -1,11 +1,25 @@
 #include <ios>
 #include <iostream>
+#include <istream>
 #include <limits>
+#include <optional>
 #include <print>
 
 unsigned const_number()
 {
 	return 42;
+}
+
+std::optional<int> read_number(std::istream& in)
+{
+	int result{};
+	if (in >> result)
+	{
+		return result;
+	}
+	in.clear();
+	in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	return {};
 }
 
 unsigned input()
@@ -37,9 +51,26 @@ void guess_number(unsigned number)
 	std::println("Well done.");
 }
 
+void guess_number_or_quit(int number)
+{
+	std::print("Guess the number. \n > ");
+	std::optional<int> guess;
+
+	while ((guess = read_number(std::cin)))
+	{
+		if (guess.value() == number)
+		{
+			std::println("Well done.");
+			return;
+		}
+		std::print("{} is wrong. Try again. \n> ", guess.value());
+	}
+	std::println("The number was {}", const_number());
+}
+
 int main()
 {
-	guess_number(const_number());
+	guess_number_or_quit(const_number());
 
 	return 0;
 }
